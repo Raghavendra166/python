641184a3ee65092c38154a85c22310386519d79a
RAGHAVENDRA <peddiraghavendraswamy@gmail.com>
2024-11-14 11:10:41 +0530

    Delete d1 (1).py

--- d1 (1).py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-data=pd.read_csv("C://Users/NAGUL/Downloads/matches.csv")
-matches_per_season = data.groupby('Season').size().reset_index(name='Match_Count')
-print(matches_per_season)
